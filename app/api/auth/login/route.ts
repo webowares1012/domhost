fa940ef6aebe57e0f775d67a2bf9ee6532721c8f
webowares1012 <webowares@gmail.com>
2026-09-04 00:35:36 +0530

    error fix

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -71,7 +71,7 @@ export async function POST(req: NextRequest) {
       value: token,
       httpOnly: true,
       // secure: process.env.NODE_ENV === "production",
-      secure: false,
+      secure: true,
       sameSite: "lax",
       path: "/",
       maxAge: 60 * 60 * 1,
